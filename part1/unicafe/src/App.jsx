719073a99e00ca22b7600857c4d3aea3d49ca8ac
Rauhayl <rauhayl.beoe@gmail.com>
2025-02-21 13:18:56 +0500

    1.9: unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,38 +1,39 @@
 import { useState } from "react"
 
 const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad;
+
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  }
+
   return (
     <div>
-       <h1> Statistics </h1>
-      
-      <p> Good: {props.good} </p> 
-      <p> Neutral: {props.neutral} </p>
-      <p> Bad: {props.bad} </p>
-
-      <p> All: {props.good + props.neutral + props.bad} </p>
-      <p> Average : {props.good + props.neutral + props.bad === 0 ? 0 : (props.good - props.bad) / (props.good + props.neutral + props.bad)} </p>
-      <p> Positive: {props.good + props.neutral + props.bad === 0 ? 0 : (props.good / (props.good + props.neutral + props.bad)) * 100} % </p>
+      <h1>Statistics</h1>
+      <p>Good: {props.good}</p>
+      <p>Neutral: {props.neutral}</p>
+      <p>Bad: {props.bad}</p>
+      <p>All: {total}</p>
+      <p>Average: {(props.good - props.bad) / total}</p>
+      <p>Positive: {(props.good / total) * 100} %</p>
     </div>
-  )
+  );
 }
 
 const App = () => {
-
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      
-      <button onClick={() => setGood(good +1)} > Good </button>
-      <button onClick={() => setNeutral(neutral +1)} > Neutral </button>
-      <button onClick={() => setBad(bad +1)} > Bad </button>
-      
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

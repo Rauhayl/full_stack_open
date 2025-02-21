8dd8572ed62dfe9e696877c99f109cd8c958e0b0
Rauhayl <rauhayl.beoe@gmail.com>
2025-02-21 13:13:58 +0500

    1.8: unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,10 +1,26 @@
 import { useState } from "react"
 
+const Statistics = (props) => {
+  return (
+    <div>
+       <h1> Statistics </h1>
+      
+      <p> Good: {props.good} </p> 
+      <p> Neutral: {props.neutral} </p>
+      <p> Bad: {props.bad} </p>
+
+      <p> All: {props.good + props.neutral + props.bad} </p>
+      <p> Average : {props.good + props.neutral + props.bad === 0 ? 0 : (props.good - props.bad) / (props.good + props.neutral + props.bad)} </p>
+      <p> Positive: {props.good + props.neutral + props.bad === 0 ? 0 : (props.good / (props.good + props.neutral + props.bad)) * 100} % </p>
+    </div>
+  )
+}
+
 const App = () => {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
-  const [bad, setbad] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
@@ -12,17 +28,9 @@ const App = () => {
       
       <button onClick={() => setGood(good +1)} > Good </button>
       <button onClick={() => setNeutral(neutral +1)} > Neutral </button>
-      <button onClick={() => setbad(bad +1)} > Bad </button>
+      <button onClick={() => setBad(bad +1)} > Bad </button>
       
-      <h1> Statistics </h1>
-      
-      <p> Good: {good} </p> 
-      <p> Neutral: {neutral} </p>
-      <p> Bad: {bad} </p>
-
-      <p> All: {good + neutral + bad} </p>
-      <p> Average : {good + neutral + bad === 0 ? 0 : (good - bad) / (good + neutral + bad)} </p>
-      <p> Positive: {good + neutral + bad === 0 ? 0 : (good / (good + neutral + bad)) * 100} % </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
